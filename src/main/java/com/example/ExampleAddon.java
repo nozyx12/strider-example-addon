9241220233b9e21c748e4562ce411c531cb16740
nozyx12 <nozyxoff@proton.me>
2026-08-24 12:05:41 +0200

    Clarified how to choose an addon's Java version + Changed StriderLoader version to 1.2.0 + Added strider-resource-bridge as a dependency

--- src/main/java/com/example/ExampleAddon.java
+++ src/main/java/com/example/ExampleAddon.java
@@ -2,21 +2,30 @@ package com.example;
 
 import dev.nozyx.strider.loader.api.Addon;
 import dev.nozyx.strider.loader.api.IStriderLoader;
+import org.apache.logging.log4j.Logger;
 
 // Main class of your addon.
 public class ExampleAddon implements Addon {
+
+    private Logger logger;
 
     @Override
     public void onInitialize(IStriderLoader loader) {
         // Called once StriderLoader has successfully loaded the addon before the game starts.
         // ( Perfect to prepare transformations with TransformationUtils or using ClassTransformers )
-        System.out.println("Hello from addon!");
+
+        logger = loader.getLogger(
+                // Change this to be the same as your addon ID.
+                "strider-example-addon"
+        );
+
+        logger.info("Hello from addon!");
     }
 
     @Override
     public void onReady(IStriderLoader loader) {
         // Called once Minecraft reached a state where registries can be used.
         // ( Perfect to register blocks, items, or more... )
-        System.out.println("Registries ready!");
+        logger.info("Registries ready!");
     }
 }
